d1b1075f7e32ffb8362b76cec8ed201dce095355
Mox90 <moctar.sandalo89@gmail.com>
2023-06-17 09:37:49 +0300

    Getting data from DB. Closes Get products from database #5

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 dotenv.config()
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 const port = process.env.PORT || 5000
 
 connectDB()
@@ -13,14 +13,9 @@ app.get('/', (req, res) => {
   res.send(`API is running...`)
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+//app.get('/api/products', productRoutes)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
