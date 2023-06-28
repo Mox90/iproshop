c37a28de71ce975a693d59d510191955cd0f28c1
Mox90 <moctar.sandalo89@gmail.com>
2023-06-28 11:41:27 +0300

    Implementing Authentication middlewara and its endpoints

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectDB from './config/db.js'
+import cookieParser from 'cookie-parser'
 dotenv.config()
+import connectDB from './config/db.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -14,6 +15,9 @@ const app = express()
 // Body parser midleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Cookie parser middleware
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send(`API is running...`)
